bd57a54b878db21770e22c54ce63ef5f06c21ea7
Bisrateab Gebrie <Bisrateab.G@ethswitch.com>
2026-02-21 08:22:34 +0300

    fix: updated next.config.mjs to use export default

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This allows the build to finish even if there are linting errors
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // If you are using TypeScript, you can also add this to skip type errors
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
